b3b9441286c4015b7237604176765a443a11f4b8
dukedk <duke91662@gmail.com>
2023-07-15 11:13:10 +0100

    edited main

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:roadside_assistance/presentation/screens/drivers/TechnicianScree
 
 // void main() => runApp(MyApp());
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,

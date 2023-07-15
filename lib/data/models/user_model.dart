e165a6b5f6dee7781cc8e673fe6e2b0a5a494ab9
Dyce <njokomalain@gmail.com>
2023-07-15 14:57:15 +0100

    sign in and signup functionality

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -1,8 +1,35 @@
 class UserModel {
   final String names;
-  final String driverId;
+  String? driverId;
   final String email;
-  final String  phoneNumber;
+  final String phoneNumber;
 
-  
+  UserModel(
+      {required this.names,
+      this.driverId,
+      required this.email,
+      required this.phoneNumber});
+
+  // tojson method
+  Map<String, dynamic> toJson() => {
+        'names': names,
+        'driverId': driverId,
+        'email': email,
+        'phoneNumber': phoneNumber,
+      };
+
+  //copywith method
+  UserModel copyWith({
+    String? names,
+    String? driverId,
+    String? email,
+    String? phoneNumber,
+  }) {
+    return UserModel(
+      names: names ?? this.names,
+      driverId: driverId ?? this.driverId,
+      email: email ?? this.email,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+    );
+  }
 }
